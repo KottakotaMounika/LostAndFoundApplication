8ea88d0ad3e54d1eb55cbc4cade5a7eab2ab4202
Mounika <mounika.kottakota123@gmail.com>
2025-10-27 19:24:47 +0530

    Initial commit

---
+++ lostAndFoundApplication/src/main/java/edu/infosys/lostAndFoundApplication/LostAndFoundApplication.java
@@ -0,0 +1,19 @@
+package edu.infosys.lostAndFoundApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFoundApplication.class, args);
+	}
+
+}
+/*
+ * username:SathvikSesha password:Sathvik@31 role : admin
+ * username : Shiva password:Shiva@31 role student
+ * username : hari password : Hari@31 role student
+ * 
+ */
